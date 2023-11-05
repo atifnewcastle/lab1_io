a45dccf9ce4cae1e5044c30468ccdce702105d41
atifnewcastle <atiframlan@gmail.com>
2023-11-05 14:55:48 +0800

    First commit

---
+++ app/src/main/java/com/example/lab1_io/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.lab1_io;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tvMessage;
+    Button submitButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //assign items in display(activity_main.xml) into a variable.
+        tvMessage = (TextView) findViewById(R.id.textView);
+        submitButton = (Button) findViewById(R.id.button);
+    }
+    public void displayText(View view){
+        tvMessage.setText("Welcome to ICT602 OCT 2023");
+    }
+
+
+}
